cfb6ebfa201f79e8444b1e5d9838f68b2f53879b
Vitor Silva <vitorgilcs@gmail.com>
2020-01-04 10:33:45 +0000

    Build a Database Web App - Spring MVC and Hibernate project - part 3

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,29 +5,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.services.CustomerServices;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	//need to inject the customer dao
+	//need to inject the customer service
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerServices customerService;
 	
-	@RequestMapping("/list")
+		
+	@GetMapping("/list")
 	public String listCustomers(Model theModel) {
 		
-		//get customers from dao
-		List<Customer> theCustomers = customerDAO.getCustomers();
+		//get customers from the Service
+		List<Customer> theCustomers = customerService.getCustomers();
 		
 		//add the customers to the model
 		theModel.addAttribute("customers", theCustomers);
-		
-		
+				
 		return "list-customers";
 	}
 }
